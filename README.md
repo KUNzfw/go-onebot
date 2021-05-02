ba908b9a0df3b4d366410e4554958557427a5396
KUNzfw <zhuofengwang2003@gmail.com>
2021-05-02 17:59:14 +0800

    添加示例代码

--- README.md
+++ README.md
@@ -1,30 +1,4 @@
 # go-onebot
 
 ## 快速上手
-```go
-// 一个简单的私聊复读机
-package main
-
-import (
-	"log"
-
-	"github.com/KUNzfw/go-onebot/onebot"
-)
-
-func main() {
-	bot := onebot.CreateWsBot("ws://localhost:6700/", nil)
-	handler := onebot.EventHandler{
-		OnPrivateMessage: func(data *onebot.EventPrivateMessage) {
-			log.Printf("收到来自 %v (%v) 的私聊消息: %v\n", data.Sender.Nickname, data.Sender.UserID, data.Message)
-			msgID, err := bot.SendPrivateMessage(data.Sender.UserID, data.Message, false)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("给 %v (%v) 发送私聊消息: %v [Message ID: %v]", data.Sender.Nickname, data.Sender.UserID, data.Message, msgID)
-		},
-	}
-	if err := bot.HandleEvent(&handler); err != nil {
-		log.Fatal(err)
-	}
-}
-```
+[私聊复读机](/example/echo.go)
---
+++ example/echo.go
@@ -0,0 +1,30 @@
+// 一个简单的私聊复读机
+package main
+
+import (
+	"log"
+
+	"github.com/KUNzfw/go-onebot/onebot"
+)
+
+func main() {
+	bot := onebot.CreateWsBot("ws://localhost:6700/", nil)
+
+	if info, err := bot.GetLoginInfo(); err == nil {
+		log.Printf("登录的机器人是 %v (%v)", info.NickName, info.UserID)
+	}
+
+	handler := onebot.EventHandler{
+		OnPrivateMessage: func(data *onebot.EventPrivateMessage) {
+			log.Printf("收到来自 %v (%v) 的私聊消息: %v\n", data.Sender.Nickname, data.Sender.UserID, data.Message)
+			msgID, err := bot.SendPrivateMessage(data.Sender.UserID, data.Message, false)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Printf("给 %v (%v) 发送私聊消息: %v [Message ID: %v]", data.Sender.Nickname, data.Sender.UserID, data.Message, msgID)
+		},
+	}
+	if err := bot.HandleEvent(&handler); err != nil {
+		log.Fatal(err)
+	}
+}
