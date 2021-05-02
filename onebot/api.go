f2b6010e5d376c2c9aa937cdd3c42218f7dac40d
KUNzfw <zhuofengwang2003@gmail.com>
2021-05-02 17:58:59 +0800

    实现GetLoginInfo

--- onebot/api.go
+++ onebot/api.go
@@ -1,5 +1,7 @@
 package onebot
 
+import "github.com/mitchellh/mapstructure"
+
 // SendPrivateMessage 发送私聊消息
 func (bot *Bot) SendPrivateMessage(userID int64, message string, autoEscape bool) (messageID int32, err error) {
 	resp := make(map[string]interface{})
@@ -16,5 +18,22 @@ func (bot *Bot) SendPrivateMessage(userID int64, message string, autoEscape bool
 		// 根据onebot标准，这里的强制转换没有问题
 		messageID = int32(id)
 	}
+	return
+}
+
+type LoginInfo struct {
+	UserID   int64  `mapstructure:"user_id"`
+	NickName string `mapstructure:"nickname"`
+}
+
+// GetLoginInfo 获取登录号信息
+func (bot *Bot) GetLoginInfo() (info *LoginInfo, err error) {
+	resp := make(map[string]interface{})
+	if cerr := bot.caller.Call("get_login_info", nil, &resp); cerr != nil {
+		return nil, cerr
+	}
+
+	info = &LoginInfo{}
+	err = mapstructure.Decode(resp, info)
 	return
 }
